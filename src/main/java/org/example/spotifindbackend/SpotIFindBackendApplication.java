783ac0d54dee6d9672a1c3fc270e387e00c4c9d1
zahid <309906859+zdz05@users.noreply.github.com>
2026-08-04 18:34:58 +0100

    inital commit

---
+++ src/main/java/org/example/spotifindbackend/SpotIFindBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.spotifindbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotIFindBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotIFindBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spotifindbackend/SpotIFindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spotifindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotIFindBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
